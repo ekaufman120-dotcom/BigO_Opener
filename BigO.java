e031598eae36e1c3e628fdf78155c67719001cac
Kaufman <291kaufmane@student.dist113.org>
2026-09-08 09:16:24 -0500

    Made the error actually print

--- BigO.java
+++ BigO.java
@@ -17,28 +17,32 @@ public class BigO{
         int missingNumber = -1;
         int blankSpace = -1;
         
+        //Throws an exception if the array is not of length 10.
         if(theNumbers.length != 10){
             try{
                 throw new IncorrectArrayException("Array must be of length 10.");
             } catch(IncorrectArrayException e){
-                e.getMessage();
+                System.out.println(e.getMessage());
             }
         }
 
+        //Loops through the array to find the index of the zero.
         for(int i = 0; i < theNumbers.length; i++){
             if(theNumbers[i] == 0){
                 blankSpace = i;
             }
         }
 
+        //Throws an exception if the array does not have a zero.
         if(blankSpace == -1){
             try{
                 throw new IncorrectArrayException("Array must contain a zero to indicate the missing number.");
             } catch(IncorrectArrayException e){
-                e.getMessage();
+                System.out.println(e.getMessage());
             }
         }
-
+        
+        //Loops through 1-10 to find which number does not appear in the array.
         for(int i = 1; i <=10; i++)
         {
             boolean foundNum = false;
@@ -47,6 +51,15 @@ public class BigO{
                 if(theNumbers[j] == i){
                     foundNum = true;
                     break;
+                }
+
+                //If a number is out of the range [0, 10], the program throws an exception.
+                if(theNumbers[j] <0 || theNumbers[j] > 10){
+                    try{
+                        throw new IncorrectArrayException("Array must only contain numbers between 0 and 10.");
+                    } catch(IncorrectArrayException e){
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
             if(!foundNum){
