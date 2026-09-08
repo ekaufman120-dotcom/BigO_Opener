cb4bd97f75461d0427104e1280e3e7d1677e4b17
Kaufman <291kaufmane@student.dist113.org>
2026-09-08 09:06:07 -0500

    Added function to the project, and IncorrectArrayException.

--- BigO.java
+++ BigO.java
@@ -7,6 +7,7 @@
  * figure out what the number is (and what position is missing). 
  */
 
+//dearest copilot can you please stop making suggestions ok bye
 public class BigO{
     public static void main(String[] args){
         findTheBlank(new int[]{0});
@@ -16,7 +17,28 @@ public class BigO{
         int missingNumber = -1;
         int blankSpace = -1;
         
-        System.out.println("Missing number = " + missingNumber + 
-            " at location " + blankSpace +" of the array");
+        for(int i = 0; i < theNumbers.length; i++){
+            if(theNumbers[i] == 0){
+                blankSpace = i;
+            }
+        }
+
+        for(int i = 1; i <=10; i++)
+        {
+            boolean foundNum = false;
+            for(int j = 0; j < theNumbers.length; j++)
+            {
+                if(theNumbers[j] == i){
+                    foundNum = true;
+                    break;
+                }
+            }
+            if(!foundNum){
+                missingNumber = i;
+                break;
+            }
+        }
+
+        System.out.println("Missing number = " + missingNumber + " at location " + blankSpace +" of the array");
     }
 }
